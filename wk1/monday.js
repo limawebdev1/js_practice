5cc236ced0dd4f5d7c5332eaae0bc0abf579a96a
Lisa Ma <limawebdev@gmail.com>
2017-09-11 12:15:32 -0600

    added wk1 monday challenge

---
+++ wk1/monday.js
@@ -0,0 +1,28 @@
+/* 
+    Given an array of different data types: 
+    e.g. ['Sup', 34, 419.2, ['hi', 'there'], { a : b, c : d }, null, ...], 
+
+    Write a function that returns an Object that has keys representing the type of data,
+    and values that represent the subset of values within the input array that are of 
+    that data type:
+    
+    e.g. 
+    {
+        'String' : [ 'Sup' ], 
+        'Array' : [ ['hi', 'there'] ], 
+        'Number' : [ 34, 419.2 ], 
+        'Object' : [ { a : b, c : d} ], 
+        ...
+        ...
+        ...
+    }
+
+    Please provide me with at least 3 tests using console.log(dataTypeSort(arr)) with 3 different inputs. 
+    If possible, document your approach to the problem with comments.  
+*/
+
+
+
+function dataTypeSort (arr) {
+
+}
