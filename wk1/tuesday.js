4db93ce7ced427e2a2c6b2547079cb66b59b13dc
Lisa Ma <limawebdev@gmail.com>
2017-09-12 12:26:49 -0600

    week 1 tuesday challenge up

---
+++ wk1/tuesday.js
@@ -0,0 +1,28 @@
+/* 
+    Given an object with String key-value pairs like such: 
+    
+    {
+        'hey' : 'dude', 
+        'how' : 'are', 
+        'you': 'doing', 
+        ...
+        ...
+    }
+
+    Write a function that returns an array with two inner arrays, 
+    the first being an array of the alphabetically sorted keys
+    and the second being an array of reverse-alphabetically sorted values.
+
+    e.g.
+    [ ['hey', 'how', 'you'], ['dude', 'doing', 'are'] ]
+
+    Please provide me with at least 3 unique tests using console.log(alphabeticalObjectSort(obj)).
+    Document your approach to the problem with comments.
+
+*/
+
+
+
+function alphabeticalObjectSort(obj) {
+
+}
