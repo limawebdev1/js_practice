63df102776c36e3b6b711e9623a2bfccb286e12e
Lisa Ma <limawebdev@gmail.com>
2017-09-13 10:14:29 -0600

    wednesday week 1 challenge up

--- wk1/tuesday.js
+++ wk1/tuesday.js
@@ -21,8 +21,6 @@
 
 */
 
-
-
 function alphabeticalObjectSort(obj) {
 
 }
---
+++ wk1/wednesday.js
@@ -0,0 +1,23 @@
+/* 
+    Given an array of positive integers: 
+    
+    [ 1, 4, 5, 7, 1, 39, 148, ... ]
+
+    Write a function that returns the sum of only the odd elements in the array. 
+    Write a function that returns the sum of only the even elements in the array. 
+
+    e.g.
+    oddSum([ 1, 4, 5, 7, 1, 39, 148 ]) --> 1 + 5 + 7 + 1 + 39 --> returns 53. 
+
+    Please provide me with at least 3 unique tests using console.log(oddSum(arr), evenSum(arr)).
+    Document your approach to the problem with comments.
+
+*/
+
+function oddSum(arr) {
+
+}
+
+function evenSum(arr) {
+
+}
