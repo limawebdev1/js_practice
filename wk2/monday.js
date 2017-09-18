1f88d3926da6ecf6d01afcdc96576bdabd76952a
Lisa Ma <limawebdev@gmail.com>
2017-09-18 11:41:34 -0600

    wk2 monday challenge up

---
+++ wk2/monday.js
@@ -0,0 +1,27 @@
+/*
+    Given an array of letters in the alphabet, that can be uppercase or lowercase. 
+    e.g. [ 'a', 'C', 'D', 'z', 'A', 't', 'a', 'c', ... ]
+
+    Write a function that returns an Object where the keys are the lowercase version 
+    of letters in the array, and the values are the number of times that letter comes up 
+    regardless of case.
+
+    e.g. 
+    Given: [ 'a', 'C', 'D', 'z', 'A', 't', 'a', 'c' ], your function would return 
+
+    {
+        'a' : 3, (so count up all the occurences of 'a' regardless of case e.g. 'a' or 'A')
+        'c' : 2, 
+        'd' : 1, 
+        'z' : 1, 
+        't' : 1        
+    }
+
+    Please provide me with at least 3 unique tests using console.log(countAlphabet(arr)).
+    Document your approach to the problem with comments.
+*/
+
+
+function countAlphabet(arr) {
+    
+}
