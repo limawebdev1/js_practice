d459ab83a35e236d436931fcf392ad1fba09fd5d
Lisa Ma <limawebdev@gmail.com>
2017-09-19 12:20:17 -0600

    tuesday challenge wk2 up

---
+++ wk2/tuesday.js
@@ -0,0 +1,25 @@
+/*
+    Given an array of Strings
+    e.g. [ 'fwleif', '(249) 429-2913', '1235832817', '1k2if', '4jffa9ejg1', '149-294-1938', ... ]
+
+    Write a function that returns an Array of only "valid" phone numbers (any 10 digit sequence is
+    considered valid)
+
+    e.g. 
+    Given: e.g. [ 'fwleif', '(249) 429-2913', '1235832817', '2j3j1j4i2u4hu3huo1ji3il4', '4jffa9ejg1', '149-294-1938'],
+    your function would return:
+
+    [ '(249) 429-2913', '1235832817', '149-294-1938', '2j3j1j4i2u4hu3huo1ji3il4']
+
+    Unconventional examples of "valid" phone numbers are: '2j3j1j4i2u4hu3huo1ji3il4' and '13924-19248'
+    For the purposes of this challenge, these are also considered valid as long as there are ten
+    digits in the String.
+
+    Please provide me with at least 3 unique tests using console.log(questionablyValidPhoneNumber(arr)).
+    Document your approach to the problem with comments.
+*/
+
+
+function questionablyValidPhoneNumber(arr) {
+
+}
