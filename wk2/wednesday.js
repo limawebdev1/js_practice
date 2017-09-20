aa4e11904871138725c761d89a90ba702cb47bab
Lisa Ma <limawebdev@gmail.com>
2017-09-20 15:16:52 -0600

    Wednesday wk2 challenge up

---
+++ wk2/wednesday.js
@@ -0,0 +1,27 @@
+/*
+    Given an array of different files:
+    [ 'abc.txt', 'hello.exe', 'Sketch.app', ...]
+
+    Write a function that returns an object that has keys representing
+    the different file extensions and values that represent an array
+    of those files. 
+
+    e.g. Given: [ 'abc.txt', 'hello.exe', 'Sketch.app', 'file.txt', 'photo.jpeg', 'program.exe' ],
+    Your function should return:
+    {
+        'txt' : [ 'abc.txt', 'file.txt' ], 
+        'exe' : [ 'hello.exe', 'program.exe' ], 
+        'app' : [ 'Sketch.app' ], 
+        'jpeg' : [ 'photo.jpeg' ]
+    }
+
+    Keep in mind that file extensions could be more varied than the ones included in 
+    this example. 
+
+    Please provide me with at least 3 unique tests using console.log(fileSort(arr)).
+    Document your approach to the problem with comments.
+*/
+
+function fileSort(arr) {
+    
+}
